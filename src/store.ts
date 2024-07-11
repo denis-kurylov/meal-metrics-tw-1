3013cd707f0ae7e2615d288dc8d6293dae9c5b6e
Denis Kurylyov <otrawaotrawa@gmail.com>
2024-07-11 23:45:05 +0300

    added json server, npm scripts, .gitignore, .env

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { api } from './api'
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(api.middleware)
+  },
 })
